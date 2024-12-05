dcb8b6be0114db96f19569b0ebffd449279e989c
Simon <106014761+simonhoumb@users.noreply.github.com>
2024-12-05 02:04:46 +0100

    feat: Added day1 challenge

---
+++ day1/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	leftList := make([]int, 0)
+	rightList := make([]int, 0)
+	distanceList := make([]int, 0)
+	sumDistance := 0
+
+	file, err := os.Open("./day1/data.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		// processing each line
+		line := strings.Fields(scanner.Text())
+		for i, v := range line {
+			num, _ := strconv.Atoi(v)
+			if i == 0 {
+				leftList = append(leftList, num)
+			} else {
+				rightList = append(rightList, num)
+			}
+		}
+	}
+
+	sort.Ints(leftList)
+	sort.Ints(rightList)
+
+	for i := 0; i < len(leftList); i++ {
+		sum := leftList[i] - rightList[i]
+		if sum < 0 {
+			sum = -sum
+		}
+		distanceList = append(distanceList, sum)
+	}
+
+	for _, v := range distanceList {
+		sumDistance = sumDistance + v
+	}
+
+	fmt.Println("The total distance and the answer for day 1 is: ", sumDistance)
+}
